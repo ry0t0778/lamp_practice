27329b0c1b58944d5db10598cd95b8ad06603ca0
ryoto omura <tyorotyoro20102@gmail.com>
2021-07-15 14:39:10 +0900

    CSRF対策

--- www/html/finish.php
+++ www/html/finish.php
@@ -14,15 +14,16 @@ if (is_logined() === false) {
 $db = get_db_connect();
 $user = get_login_user($db);
 
-$carts = get_user_carts($db, $user['user_id']);
-
 if (is_valid_csrf_token($token)) {
+  $carts = get_user_carts($db, $user['user_id']);
+
   if (purchase_carts($db, $carts) === false) {
     set_error('商品が購入できませんでした。');
     redirect_to(CART_URL);
   }
 } else {
   set_error('不正な操作が行われました');
+  redirect_to(CART_URL);
 }
 $total_price = sum_carts($carts);
 
